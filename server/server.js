b38c17433e04d8f5e1f19808da903be5286643ec
Prakash Thakur <prakashthakur0021@gmail.com>
2022-06-09 19:07:54 +0530

    Chat app using socket.io

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const http = require('http')
+const { Server } = require('socket.io');
+const cors = require('cors')
+app.use(cors());
+
+
+
+const server = http.createServer(app);
+
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`User connected: ${socket.id}`)
+
+
+    socket.on('join_room', (data) => {
+        socket.join(data);
+        console.log(`User Id: ${socket.id} joined Room: ${data}`);
+    })
+
+    socket.on("send_message", data => {
+        socket.to(data.room).emit("receive_message", data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`)
+    })
+
+});
+
+server.listen(3001,() => {
+    console.log('Server is running!');
+})
